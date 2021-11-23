f675783a51a5f3a9f2a40f61d4bb67c3c728c8b6
alejo <Alejo Moyano>
2021-11-23 19:39:43 -0300

    prueba1

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def get_power(text: str) -> None:
 
             is_number = False
     
-    #return 'Total power draw:' + str(result) + 'W'
     return result
 
+# metodo para revisar la potencia del rig
 def callback_hour(context: CallbackContext) -> None:
     command = "nvidia-smi --query-gpu=power.draw --format=csv | grep '[0-9]'"
     chat_id = -1001775199662
@@ -37,38 +37,54 @@ def callback_hour(context: CallbackContext) -> None:
     p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     for line in p.stdout.readlines():
         text += str(line)
-    
-    counter+1
+
+    global counter
+    counter+=1
 
     result = get_power(text)
     if result < 480:
-        context.bot.send_message(chat_id=chat_id, text='Reiniciar el rig. Power draw: '+str(result)+'W')
+        context.bot.send_message(chat_id=chat_id, text='POTENCIA BAJA -> '+'Power draw: '+str(result)+'W')
     else:
-        if result % 5 == 0:
+        # enviamos el mensaje a telegram cada 50 min
+        if counter % 5 == 0:
             context.bot.send_message(chat_id=chat_id, text='Total power draw: '+str(result)+'W')
-            
 
-def callback_tenm(context: CallbackContext) -> None:
-    command = "nvidia-smi --query-gpu=power.draw --format=csv | grep '[0-9]'"
-    chat_id = -1001775199662
-    text = ""
-    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    for line in p.stdout.readlines():
-        text += str(line)
-
-    result = get_power(text)
-        
-    context.bot.send_message(chat_id=chat_id, text=result)
+#def callback_tenm(context: CallbackContext) -> None:
+#    command = "nvidia-smi --query-gpu=power.draw --format=csv | grep '[0-9]'"
+#    chat_id = -1001775199662
+#    text = ""
+#    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#    for line in p.stdout.readlines():
+#        text += str(line)
+#
+#    result = get_power(text)
+#        
+#    context.bot.send_message(chat_id=chat_id, text=result)
 
 def start(update: Update, context: CallbackContext) -> None:
     context.job_queue.run_repeating(callback_hour, interval=600, first=5)
-#    context.job_queue.run_repeating(callback_tenm, interval=600, first=5)
-    #print(context._chat_id_and_data)
+
+# metodo de reinicio del rig
+def restart(context: CallbackContext) -> None:
+    command = "reboot"
+    chat_id = -1001775199662
+    context.bot.send_message(chat_id=chat_id, text="Reiniciando el RIG...")
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+
+
+
+def init(context: CallbackContext) -> None:
+    context.job_queue.run_once(restart)
+
 
 def main() -> None:
     token = "2013997999:AAEjtfHPYNBmGHjflmWD2dzxLEU8l3_hvFc"
     updater = Updater(token)
+    # comando para iniciar el bot
     updater.dispatcher.add_handler(CommandHandler("start", start))
+    # comando para reiniciar el rig
+    updater.dispatcher.add_handler(CommandHandler("restart", restart))
     updater.start_polling()
     updater.idle()
 
---
+++ pruebas.py
@@ -0,0 +1,17 @@
+counter = 0
+
+def sumador():
+    global counter
+    counter+=1
+    counter+=1
+    counter+=1
+    counter+=1
+    print(counter)
+
+sumador()
+
+#def main() -> None:
+#    sumador()
+
+#if __name__ == '__main__':
+#    main()
